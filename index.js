eb38e645b2c7d5d9ffa8f2736b0ae5e22e0a3b43
Victor Nguyo <victor.nguyo@student.moringaschool.com>
2024-04-01 09:24:56 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: "Mike",
+    streetAddress: "7 Kikuyu Rd",
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee,"name","Sam");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj;
+}
+
+const secondNewEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee,"name",'Fish');
+
+function deleteFromEmployeeByKey(employee,key){
+    const editedEmployee = Object.assign({}, employee);
+    delete editedEmployee[key];
+    return editedEmployee;
+}
+
+const editedEmployee = deleteFromEmployeeByKey(employee, "streetAddress");
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
+
+const revisedEmployee = destructivelyDeleteFromEmployeeByKey(employee,"name");
